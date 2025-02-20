04785b53ab465538d7cbb050661d8d3d848dc2c2
viktor <kn1b21.kniahnytskyi@kpnu.edu.ua>
2025-02-20 21:23:07 +0200

    add button in carhistory

--- src/app/pages/user/carhistory/carhistory.component.ts
+++ src/app/pages/user/carhistory/carhistory.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { UserService } from 'src/app/modules/user/services/user.service';
+import { FormService } from 'src/app/core/modules/form/form.service';
+import { CarrecordService } from 'src/app/modules/carrecord/services/carrecord.service';
+import { Carrecord } from 'src/app/modules/carrecord/interfaces/carrecord.interface';
+import { carrecordFormComponents } from 'src/app/modules/carrecord/formcomponents/carrecord.formcomponents';
+import { TranslateService } from 'src/app/core/modules/translate/translate.service';
+import { Router } from '@angular/router'; // додано для навігації
+import { UserService } from 'src/app/modules/user/services/user.service'; // Імпортуємо UserService, якщо потрібно
+import { log } from 'console';
 
 @Component({
 	templateUrl: './carhistory.component.html',
@@ -9,9 +16,35 @@ import { UserService } from 'src/app/modules/user/services/user.service';
 export class CarhistoryComponent {
 	isMenuOpen = false;
 
-	constructor(public userService: UserService) {}
+	constructor(
+		public userService: UserService,
+		private _form: FormService,
+		private _carrecordService: CarrecordService,
+		private _translate: TranslateService,
+		private _router: Router
+	) {}
 
 	back(): void {
-		window.history.back();
+		window.history.back();  // функція для повернення на попередню сторінку
+	}
+
+	create(): void {
+		this._form.modal<Carrecord>(this._form.getForm('carrecord', carrecordFormComponents), {
+			label: 'Create Carrecord',
+			click: (created: unknown, close: () => void) => {
+				close();
+
+				const carrecord = created as Carrecord;
+				carrecord.__created = false;
+
+				// якщо потрібно, можна додати інші дані до carrecord
+				// carrecord.car = someCarId;  // Якщо потрібно передати car_id
+
+				this._carrecordService.create(carrecord).subscribe(() => {
+					// після створення, перенаправляємо користувача на певну сторінку
+					this._router.navigate(['/сarrecord']);
+				});
+			},
+		});
 	}
 }
--- src/app/pages/user/carhistory/carhistory.module.ts
+++ src/app/pages/user/carhistory/carhistory.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: '',
+		path: ':car_id',
 		component: CarhistoryComponent
 	}
 ];
