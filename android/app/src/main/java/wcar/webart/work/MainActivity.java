332602b967d9417b4f571935cc33aa7480a2e01f
viktor <kn1b21.kniahnytskyi@kpnu.edu.ua>
2025-02-24 16:35:13 +0200

    added proped assets

---
+++ android/app/src/main/java/wcar/webart/work/MainActivity.java
@@ -0,0 +1,5 @@
+package wcar.webart.work;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wcar.webart.work',
+  appName: 'wCar',
+  webDir: 'dist/app/browser'
+};
+
+export default config;
